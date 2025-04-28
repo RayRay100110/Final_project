3b62edc1262263a8c4366e831b8e47414c868528
RayRay100110 <rayannamorton@gmail.com>
2025-04-28 11:52:14 -0500

    Add files via upload

---
+++ app.h
@@ -0,0 +1,16 @@
+/*
+ * app.h
+ *
+ *  Created on: Apr 25, 2025
+ *      Author: rsm0195
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "stm32l4xx_hal.h"
+
+void App_Init(void);
+void App_MainLoop(void);
+
+#endif /* INC_APP_H_ */
